9b1653dcae48d7a6a56eda4932aec3e99a6ee41e
Evan Ilukhin <evanilukhin@gmail.com>
2019-07-05 22:20:27 +0300

    Fix absent cookie

--- app/controllers/api/jwt_cookies/users_controller.rb
+++ app/controllers/api/jwt_cookies/users_controller.rb
@@ -5,7 +5,7 @@ module Api
       before_action :find_user
 
       def name
-        if @current_user.present? && @current_user.is_a?(User)
+        if @current_user && @current_user.is_a?(User)
           render json: {name: @current_user.name}
         else
           render json: {message: 'Bad user'}, status: 401
@@ -15,9 +15,14 @@ module Api
       private
 
       def find_user
-        token = JSON.parse(session[:jwt_cookies])['jwt']
-        user_id = Knock::AuthToken.new(token: token).payload["sub"]
-        @current_user = User.find_by(id: user_id)
+        jwt_cookies = session[:jwt_cookies]
+        if jwt_cookies.present?
+          token = JSON.parse(jwt_cookies)['jwt']
+          user_id = Knock::AuthToken.new(token: token).payload["sub"]
+          @current_user = User.find_by(id: user_id)
+        else
+          render json: {message: 'Token not found'}, status: 401
+        end
       end
     end
   end
